304d235bfa1755922f36b06836ba68b0d8020b15
kristapsmac <40638065+kristapsmac@users.noreply.github.com>
2018-07-18 15:07:02 +0300

    web

---
+++ js/defaultJava.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", function () {
+    window.cookieconsent.initialise({
+        "palette": {
+            "popup": {
+                "background": "#000"
+            },
+            "button": {
+                "background": "#f1d600"
+            }
+        },
+        "theme": "classic",
+        "position": "bottom-right",
+        "content": {
+            "href": "file:///C:/Users/Kristaps/Documents/galadarbs/default.html"
+        }
+    })
+});
